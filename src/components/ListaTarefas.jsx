3ccce14eaa6cfbf26d8a5c40b160ed8774c291a8
LuanaValle <luanavalle.mendes@gmail.com>
2022-06-03 18:05:08 -0300

    Separação components

---
+++ src/components/ListaTarefas.jsx
@@ -0,0 +1,134 @@
+import {useState, useEffect} from 'react';
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs';
+import {API} from '../services/Api';
+export function ListaTarefas() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //Load toDos on page load
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    await fetch(API + "/todos/", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => [...prevState, todo]);
+
+    setTitle("");
+    setTime("");
+  };
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  };
+
+  // cod alterar, editar tarefa
+
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) =>
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  };
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <>
+      <div className="todo-header">
+        <h1>React ToDoList</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="Tempo estimado (em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa" />
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2>Lista de Tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+              // botão alterar tarefa
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
